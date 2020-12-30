686a18c936ef69c62b5ca0c1bd036fcedc50c5ce
JhanaGUSTAVE <jhana.gustave@gmail.com>
2020-12-30 05:20:10 -0500

    first commit

---
+++ truffle.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+        version: "0.6.0" // or find out what the latest version is and use that instead
+    }
+}
+};
